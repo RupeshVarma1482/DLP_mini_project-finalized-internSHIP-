eba8035d1d41f355b86255f722f831a8bce83da3
flins <rupeshrupu.1482@gmail.com>
2026-09-04 18:30:15 +0530

    day 1 USB integration

--- dlp_service/rough1.py
+++ dlp_service/rough1.py
@@ -7,15 +7,41 @@
 
 
 
-def func1():
-    try:
-        print(f"try block executed")
-        raise Exception("something went wrong")
-    except Exception as e:
-        print(f"manual error: {e}")
-        return {
-            "result": True,
-            "message": "except block returned this obj"
-        }
+# def func1():
+#     try:
+#         print(f"try block executed")
+#         raise Exception("something went wrong")
+#     except Exception as e:
+#         print(f"manual error: {e}")
+#         return {
+#             "result": True,
+#             "message": "except block returned this obj"
+#         }
 
-print(f"func1 return statement: {func1()}")
+# print(f"func1 return statement: {func1()}")
+
+
+
+
+
+
+from collections import namedtuple
+
+User = namedtuple("User", ["name", "vision", "weapon"])
+user1 = User("arlecchino", "pyro", "polearm")
+user2 = User("neuvillette", "hydro", "catalyst")
+user3 = User("mavuika", "pyro", "clatmore")
+print(f"user1: {user1}")
+print(f"user2: {user2}")
+print(f"user3: {user3}")
+combined = []
+combined.append(user1)
+combined.append(user2)
+combined.append(user3)
+print(f"combined: {combined}")
+
+for item in combined:
+    if "arlecchino" in item:
+        print(f"yes, she is present")
+    else:
+        print(f"no, she is not present")
--- integrations/usb/file_movement_detector.py
+++ integrations/usb/file_movement_detector.py
@@ -1,21 +1,45 @@
-import wmi
-import win32file
+# import wmi
+# import win32file
 
-def paste_operations_detection():
-    c = wmi.WMI()
-    watcher = c.Win32_VolumeChangeEvent.watch_for()
-    drive_name = None
-    while True:
-        event = watcher()
-        if event.EventType == 2:
-            print(f"drive inserted")
-            drive_name = event.DriveName
-            print(f"drive name:", drive_name)
+# def paste_operations_detection():
+#     c = wmi.WMI()
+#     watcher = c.Win32_VolumeChangeEvent.watch_for()
+#     drive_name = None
+#     while True:
+#         event = watcher()
+#         if event.EventType == 2:
+#             print(f"drive inserted")
+#             drive_name = event.DriveName
+#             print(f"drive name:", drive_name)
 
-    drive_type = win32file.GetDriveType(drive_name)
-    print(f"drive_type:", drive_type)
+#     drive_type = win32file.GetDriveType(drive_name)
+#     print(f"drive_type:", drive_type)
     
-    if drive_type == "DRIVE_REMOVABLE":
+#     if drive_type == "DRIVE_REMOVABLE":
         
 
-paste_operations_detection()
+# paste_operations_detection()
+
+
+
+
+import psutil
+import time
+
+time_var = 1
+destn_drives = []
+while time_var <= 1:
+    print(f"type of psutil.disk_partitions(): {type(psutil.disk_partitions()[0])}")
+    for drive in psutil.disk_partitions():
+        # if "fixed" in drive["opts"]:
+        if drive.opts.split(",")[1] == "fixed":
+            print(f"target drive found: {drive}")
+            print(f"target drive name: {drive.device}", end = "\n\n\n")
+            destn_drives.append(drive.device)
+            time.sleep(1)
+            time_var += 1
+
+print(f"destn_drives: {destn_drives}")
+print(f"type of destn_drives[0]: {type(destn_drives[0])}")
+
+if 
